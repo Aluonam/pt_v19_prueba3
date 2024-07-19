46fbb21bb2ff4eaa5d53f0b4b4b19c16405d2010
Paula Cano <paulacanoroman@gmail.com>
2024-07-19 10:21:26 +0200

    feat: hour and day but not setTime

--- src/components/ActualDate.tsx
+++ src/components/ActualDate.tsx
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react"
 
+
+interface localeDatPropstype {
+    day: string,
+    hour: string
+}
 const ActualDate = () => {
+
+    const [date, setDate] = useState<localeDatPropstype>();
+
+    useEffect(() => {
+      const dataDate = new Date();
+      const day = dataDate?.toLocaleDateString()
+      const hour = dataDate?.toLocaleString().split(' ')[1]
+      setDate({
+        day: day,
+        hour: hour
+      })
+    }, [])
+    
   return (
-    <div>ActualDate</div>
+    <div>
+        Hoy es: {date?.day}
+        <br></br>
+        La hora es: {date?.hour}
+        
+    </div>
+
   )
 }
 
