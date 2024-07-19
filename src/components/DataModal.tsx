5bafd4607ebaa33c12fb932720deed85a6e88803
Paula Cano <paulacanoroman@gmail.com>
2024-07-19 10:38:29 +0200

    feat: propstype

--- src/components/DataModal.tsx
+++ src/components/DataModal.tsx
@@ -1,8 +1,16 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
+
+interface dataPropstype {
+    avatar : string,
+    email: string,
+    first_name: string,
+    id: number,
+    last_name: string
+}
 
 const DataModal = () => {
 
-    // const [data, setData] = useState();
+    const [data, setData] = useState<dataPropstype>();
 
     useEffect(() => {
         callData()
@@ -13,7 +21,7 @@ const DataModal = () => {
         const call = await fetch(`https://reqres.in/api/users?page=2`);
         const dataCall = await call.json();
         console.log(dataCall.data)
-
+        setData(dataCall.data)
     }
 
 
